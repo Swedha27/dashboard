9ec19bcbe4c15989e0b4cd177980563dc0f47a32
Swedha27 <143470593+Swedha27@users.noreply.github.com>
2025-01-19 15:01:23 +0530

    Update LineChartComponent.js

--- src/components/LineChartComponent.js
+++ src/components/LineChartComponent.js
@@ -13,8 +13,8 @@ function LineChartComponent() {
         <YAxis />
         <Tooltip />
         <Legend 
-          layout="horizontal"        // Places the legend horizontally
-          verticalAlign="bottom"      // Aligns the legend below the chart
+          layout="horizontal"        
+          verticalAlign="bottom"      
           align="center"/>
       </LineChart>
     </div>
